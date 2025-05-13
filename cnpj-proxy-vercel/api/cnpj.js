f8c313b2e11cc0ab87b383e858be40c920e42530
Piixel&Code <piixelco@gmail.com>
2025-05-12 23:45:28 -0300

    Update cnpj.js

--- cnpj-proxy-vercel/api/cnpj.js
+++ cnpj-proxy-vercel/api/cnpj.js
@@ -1,4 +1,8 @@
-export default async function handler(req, res) {
+// api/cnpj.js
+
+const fetch = require("node-fetch");
+
+module.exports = async (req, res) => {
   const { cnpj } = req.query;
 
   if (!cnpj) {
@@ -15,4 +19,4 @@ export default async function handler(req, res) {
   } catch (error) {
     res.status(500).json({ error: "Erro ao buscar CNPJ" });
   }
-}
+};
